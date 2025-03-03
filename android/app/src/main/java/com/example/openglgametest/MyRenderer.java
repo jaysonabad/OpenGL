6301128e7f75362c03fe8dbe76c46b2c4c2ecba5
Jayson Abad <jayson_abad@outlook.com>
2025-03-03 21:04:51 +0800

    MyRenderer Class Fixed variable naming

--- android/app/src/main/java/com/example/openglgametest/MyRenderer.java
+++ android/app/src/main/java/com/example/openglgametest/MyRenderer.java
@@ -1,6 +1,8 @@
 package com.example.openglgametest;
 
 import android.content.Context;
+import android.opengl.GLES10;
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
 import android.view.MotionEvent;
@@ -18,8 +20,8 @@ public class MyRenderer implements GLSurfaceView.Renderer {
 
     private Context mContext;
     private FloatBuffer mVertexBuffer = null;
-    private ShortBuffer mTriangleBorderIndicesBuffer = null;
-    private int mNumOfTriangleBorderIndices = 0;
+    private ShortBuffer mIndexBuffer = null;
+    private int mIndices = 0;
     public static float mAngleX = 0.0f;
     public static float mAngleY = 0.0f;
     public float mAngleZ = 0.0f;
@@ -30,7 +32,6 @@ public class MyRenderer implements GLSurfaceView.Renderer {
     public MyRenderer(Context context) {
         mContext = context;
     }
-
 
     private void setAllBuffers(){
         float vertices[] = {
@@ -64,17 +65,17 @@ public class MyRenderer implements GLSurfaceView.Renderer {
                 7, 6, 2,
                 2, 3, 7
         };
-        ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
-        vbb.order(ByteOrder.nativeOrder());
-        mVertexBuffer = vbb.asFloatBuffer();
+        ByteBuffer vertexBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+        vertexBuffer.order(ByteOrder.nativeOrder());
+        mVertexBuffer = vertexBuffer.asFloatBuffer();
         mVertexBuffer.put(vertices);
         mVertexBuffer.position(0);
-        mNumOfTriangleBorderIndices = faces.length;
-        ByteBuffer tbibb = ByteBuffer.allocateDirect(faces.length * 2);
-        tbibb.order(ByteOrder.nativeOrder());
-        mTriangleBorderIndicesBuffer = tbibb.asShortBuffer();
-        mTriangleBorderIndicesBuffer.put(faces);
-        mTriangleBorderIndicesBuffer.position(0);
+        mIndices = faces.length;
+        ByteBuffer indexBuffer = ByteBuffer.allocateDirect(faces.length * 2);
+        indexBuffer.order(ByteOrder.nativeOrder());
+        mIndexBuffer = indexBuffer.asShortBuffer();
+        mIndexBuffer.put(faces);
+        mIndexBuffer.position(0);
     }
 
     public boolean onTouchEvent(MotionEvent e) {
@@ -103,30 +104,34 @@ public class MyRenderer implements GLSurfaceView.Renderer {
     }
 
     @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        gl.glViewport(0, 0, width, height);
+    public void onSurfaceChanged(GL10 glUnused, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
         float aspect = (float)width/height;
-        gl.glMatrixMode(GL10.GL_PROJECTION);
-        gl.glLoadIdentity();
-        gl.glFrustumf(-aspect, aspect, -1.0f, 1.0f, 1.0f, 10.0f);
+        GLES10.glMatrixMode(GL10.GL_PROJECTION);
+        GLES10.glLoadIdentity();
+        GLES10.glFrustumf(-aspect, aspect, -1.0f, 1.0f, 1.0f, 10.0f);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
+        // Set
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
+        // Perform Changes
         gl.glTranslatef(0.0f, 0.0f, -3.0f);
         gl.glRotatef(mAngleX, 1, 0, 0);
         gl.glRotatef(mAngleY, 0, 1, 0);
         gl.glRotatef(mAngleZ, 0, 0, 1);
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
         gl.glColor4f(0.5f, 0.5f, 0.9f, 1.0f);
+        // Draw
         gl.glDrawElements(
                 GL10.GL_TRIANGLES,
-                mNumOfTriangleBorderIndices,
+                mIndices,
                 GL10.GL_UNSIGNED_SHORT,
-                mTriangleBorderIndicesBuffer
+                mIndexBuffer
         );
     }
 }
+
